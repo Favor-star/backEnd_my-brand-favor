c2529469d7cec6858d1335ea73f14775244624c6
Favor-star <favoureliab@gmail.com>
2024-03-14 18:09:05 +0200

    Fixed the cors issues

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hello");
 });
@@ -32,7 +32,7 @@ app.post("*", (req, res) => {
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(3000, () => console.log("Server started successfully"));
+    app.listen(8080, () => console.log("Server started successfully"));
     console.log("Database connected successfully");
   })
   .catch((error) => console.error(error));
