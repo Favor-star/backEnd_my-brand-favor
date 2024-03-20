5603e341b49d3061059978071f39d21701730a2c
Favor-star <favoureliab@gmail.com>
2024-03-20 17:13:17 +0200

    Fised the payload issue

--- src/server.js
+++ src/server.js
@@ -22,8 +22,8 @@ app.use(
 
 app.use(express.json());
 // app.use(bodyParser({ limit: "20mb" }));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.get("/", (req, res) => {
   res.send("hello");
 });
